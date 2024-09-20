5c12effa225747ddd6c97f6ece67f0d4444ea5d0
miftah1299 <jannat27120@gmail.com>
2024-09-20 06:03:56 +0600

    linked homepage

--- js/login.js
+++ js/login.js
@@ -8,7 +8,19 @@ document
         event.preventDefault(); // prevent reloading
         console.log("Login button clicked");
 
-        // get the phone number and password
-        const phoneNumber = document.getElementById('phone').value;
-        console.log("Phone number: ", phoneNumber);
+        // get the phone number ans pin number
+        const phoneNumber = document.getElementById("phone").value;
+        const pinNumber = document.getElementById("pin").value;
+
+        console.log(phoneNumber, pinNumber);
+
+        // temporary method to check the phone number and pin number
+        if (phoneNumber === "123" && pinNumber === "1234") {
+            console.log("Login successful");
+            // redirect to the home page
+            window.location.href = 'home.html';
+        } else {
+            alert("Invalid phone number or pin number");
+            // show error message
+        }
     });
